beea0e9105e21a6b16de59aae2119959520b02b9
renars1988 <renarsozols@gmail.com>
2019-10-30 09:46:03 +0000

    fixed axios url error in contact form

--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -26,7 +26,7 @@ const ContactForm = () => {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         data: qs.stringify(values),
-        url: "/",
+        url: "/contact",
       }
 
       try {
